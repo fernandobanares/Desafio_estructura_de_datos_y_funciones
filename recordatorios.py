a57bc22c746d01fab575ab1e9581626d0a602293
Fernando <fbanarescarrasco@gmail.com>
2024-07-30 22:04:03 -0400

    segundo commit

--- recordatorios.py
+++ recordatorios.py
@@ -4,5 +4,28 @@ recordatorios = [['2021-01-01', "11:00", "Levantarse y ejercitar"],
  ['2021-09-18', "16:00", "Ramadas"],
  ['2021-12-25', "00:00", "Navidad"]]
 
-# Output
-print(recordatorios)
+
+nuevo_evento = ['2021-02-02', "06:00", "Empezar el Año"]
+recordatorios.append(nuevo_evento)
+
+recordatorios.sort()
+
+for i in range(len(recordatorios)):
+    if recordatorios[i][0] == '2021-07-15':
+        recordatorios[i][0] = '2021-07-16'
+        break
+
+for i in range(len(recordatorios)):
+    if recordatorios[i][0] == '2021-05-01':
+        del recordatorios[i]
+        break
+
+cena_navidad = ['2021-12-25', "22:00", "Cena de Navidad"]
+cena_ano_nuevo = ['2021-12-31', "22:00", "Cena de Año Nuevo"]
+recordatorios.append(cena_navidad)
+recordatorios.append(cena_ano_nuevo)
+
+recordatorios.sort()
+
+for evento in recordatorios:
+    print(evento)
--- word_count.py
+++ word_count.py
@@ -1,7 +1,15 @@
-with open("texto.txt", "r") as file:
+import sys
+
+with open(sys.argv[1], "r") as file:
     texto=file.read()
+    palabras = texto.split()
+    palabras_distintas = set(palabras)
+    num_palabras = len(palabras_distintas)
+    print(f"El número de palabras distintas es:  {num_palabras}")
+    cadena_palabras = ''.join(palabras_distintas)
+    caracteres_distintos = set(cadena_palabras)
+    num_caracteres_distintos = len(caracteres_distintos)
+    print(f"El número de caracteres distintos es: {num_caracteres_distintos}")
+       
 
-palabras = texto.count(lorem_ipsum.txt)
-
-print(f"El texto tiene {palabras} palabras")
 
